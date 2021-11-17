9e5d9bec9db9b78c9e0ca0888312373822d55ea2
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-17 17:59:49 +0300

    add class Station with all attributes and methods

--- classes/station.rb
+++ classes/station.rb
@@ -1,2 +1,23 @@
 class Station
+  attr_accessor :name, :trains
+
+  def initialize(name)
+    @trains = []
+    @name = name
+  end
+
+  def take_train(train)
+    self.trains << train
+  end
+
+  def send_train(train)
+    self.trains.delete(train)
+  end
+
+  def trains_typeof(type)
+    if type == 'cargo' || type == 'passenger'
+      self.trains.each { |train| puts train if train.type == type}
+    end
+  end
+
 end
