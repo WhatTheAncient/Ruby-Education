2e16f16eb94180d87d7f5dd2a7d18e2a1f058844
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-17 17:37:22 +0300

    add class Route with all attributes and methods

--- classes/route.rb
+++ classes/route.rb
@@ -1,2 +1,21 @@
 class Route
+  attr_accessor :stations
+  attr_reader :start, :finish
+
+  def initialize(start, finish)
+    raise TypeError unless (start.is_a?(String) && finish.is_a?(String))
+    @stations = []
+    @stations << start
+    @stations << finish
+  end
+
+  def add_station(station)
+    raise TypeError unless station.is_a? String
+    stations.insert(stations.size - 1, station) unless stations.include? station
+  end
+
+  def remove_station(station)
+    raise TypeError unless station.is_a? String
+    stations.delete(station) if stations.include? station
+  end
 end
