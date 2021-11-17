eec9e1d683be9d8eb18e082322cf32b8f3f0169a
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-17 17:20:48 +0300

    add constructor, self attributes and methods.

--- classes/train.rb
+++ classes/train.rb
@@ -1,2 +1,28 @@
 class Train
+  attr_accessor :speed, :wagons
+  attr_reader :id, :type
+
+  def initialize(id, type)
+    @id = id
+    if type == 'cargo' || type == 'passenger'
+      @type = type
+    else
+      puts "Train can be only passenger or cargo."
+    end
+  end
+
+  def stop
+    self.speed = 0
+  end
+
+  def add_wagon
+      self.wagons += 1 if speed == 0
+  end
+
+  def remove_wagon
+    if wagons > 0
+      self.wagons -= 1 if speed == 0
+    end
+  end
+
 end
