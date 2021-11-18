850f21aa3b915808d747de4d7f99648c1f4cca8c
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-18 22:42:25 +0300

    create template for work

---
+++ classes/cargo_train.rb
@@ -0,0 +1,2 @@
+class CargoTrain
+end
---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative 'classes/train'
+require_relative 'classes/route'
+require_relative 'classes/station'
+require_relative 'classes/cargo_train'
+require_relative 'classes/cargo_wagon'
+require_relative 'classes/passenger_train'
+require_relative 'classes/passenger_wagon'
+require_relative 'classes/wagon'
+
