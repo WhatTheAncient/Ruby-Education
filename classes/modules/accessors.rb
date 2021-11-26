65d90703d249006e0c7b417501d14a992df1f1db
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-26 21:14:40 +0300

    create template for module Accessors

---
+++ classes/modules/accessors.rb
@@ -0,0 +1,12 @@
+module Accessors
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+  module ClassMethods
+
+  end
+  module InstanceMethods
+
+  end
+end
